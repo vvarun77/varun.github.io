ce9d6584feff1adce23be8ea0b0917563dba9494
Varun <vvijayababu4334@gmail.com>
2025-02-22 16:12:31 -0800

    aaa

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { MainContent } from "./mainContent";
-
 function main() {
   const app = document.getElementById("app");
 
@@ -47,6 +45,24 @@ function main() {
     });
 
     return svg;
+  }
+
+  function MainContent() {
+    const content = document.createElement("div");
+    content.id = "mainContent";
+    content.className = "content";
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "I'm Varun :)";
+
+    const p = document.createElement("p");
+    p.textContent =
+      "I'm an Electrical and Computer Engineering major at the University of Washington. You can view my work below!";
+
+    content.appendChild(h1);
+    content.appendChild(p);
+
+    return content;
   }
 
   function Splash() {
